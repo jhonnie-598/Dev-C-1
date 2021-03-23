667532fcc9c1a84c424dfd40ee8f0f41128d605f
jhonier-598 <80165185+jhonnie-598@users.noreply.github.com>
2021-03-23 17:51:21 -0500

    Add files via upload

---
+++ programa 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main () 
+{
+	float distancia;
+	float tiempo;
+	float velocidad;
+cout<< "ingrese o escriba el valor distancia=";
+cin>> distancia;
+cout<<"ingrese o escriba el valor de tiempo=";
+cin>>tiempo;
+velocidad= distancia/tiempo;
+cout<< "la velocidad de una particula es=";
+cout<< velocidad;
+
+return 0;
+}
